a5a6a7e7627bab48f40c816f4c6581fe04e512e2
AV912 <88800484+AV912@users.noreply.github.com>
2024-01-28 03:05:03 -0600

    build

--- wingmates_backend/match_score.py
+++ wingmates_backend/match_score.py
@@ -1,46 +1,75 @@
-import scipy.io as sio
-import numpy as np
-import pandas as pd
+# import scipy.io as sio
+# import numpy as np
+# import pandas as pd
 
+import pickle
+import math
 import sys
 
+"""
 dot_vals = np.clip(sio.loadmat('matlab/dot_vals.mat')['dot_vals'], -1, 1)
 lang_vals = np.clip(sio.loadmat('matlab/language_dots.mat')['dot_vals'], -1, 1)
+"""
 
-countries = pd.read_csv("countries.csv")
-languages = pd.read_csv("languages.csv")
+# load dot_vals from pickle file
+with open('dot_vals.pkl', 'rb') as f:
+    dot_vals = pickle.load(f)
+
+# load lang_vals from pickle file
+with open('lang_vals.pkl', 'rb') as f:
+    lang_vals = pickle.load(f)
+
+
+# countries = pd.read_csv("countries.csv")
+# languages = pd.read_csv("languages.csv")
+    
+# load countries from pickle file
+with open('countries_dict.pkl', 'rb') as f:
+    countries_dict = pickle.load(f)
+
+# load languages from pickle file
+with open('languages_dict.pkl', 'rb') as f:
+    languages_dict = pickle.load(f)
 
 
 def country_dropdown_index(country):
-    return countries[countries['dropdown_name'] == country].index[0]
+    # return countries[countries['dropdown_name'] == country].index[0]
+    return countries_dict[country]
 
 
 def language_dropdown_index(language):
-    return languages[languages['Language'] == language].index[0]
+    # return languages[languages['Language'] == language].index[0]
+    return languages_dict[language]
 
 
 def country_match(c1, c2):
-    return (1 - np.arccos(dot_vals[c1][c2])/np.pi)**0.8
+    return (1 - math.acos(dot_vals[c1][c2])/math.pi)**0.8
 
 
 def language_match(l1, l2):
-    vals = np.array([[(1-np.arccos(lang_vals[i][j])/np.pi)**2 for i in range(len(l1))] for j in range(len(l2))])
-    return np.amax(vals)
+    # vals = [[(1-math.acos(lang_vals[i][j])/np.pi)**2 for i in range(len(l1))] for j in range(len(l2))]
+    max_val = 0
+    for i in range(len(l1)):
+        for j in range(len(l2)):
+            val = (1-math.acos(lang_vals[l1[i]][l2[j]])/math.pi)**2
+            if lang_vals[l1[i]][l2[j]] > max_val:
+                max_val = val
+    return max_val
 
 
 def gender_match(g1, g2):
     if g1 == g2:
         return 1
     elif g1 == "female" and g2 == "male":
-        return 0.95
+        return 0.90
     elif g1 == "male" and g2 == "female":
-        return 0.95
+        return 0.90
     elif g1 == "other" or g2 == "other":
         return 0.95
 
 
 def age_match(a1, a2):
-    return np.exp(-np.abs(a1-a2)/360)
+    return math.exp(-abs(a1-a2)/60)
 
 
 def match(p1, p2):
@@ -50,8 +79,8 @@ def match(p1, p2):
     c1 = country_dropdown_index(c1)
     c2 = country_dropdown_index(c2)
     
-    l1 = np.array(list(map(language_dropdown_index, l1)))
-    l2 = np.array(list(map(language_dropdown_index, l2)))
+    l1 = list(map(language_dropdown_index, l1))
+    l2 = list(map(language_dropdown_index, l2))
 
     return int(100*(country_match(c1, c2) * gender_match(g1, g2) * age_match(a1, a2) * language_match(l1, l2))**1)
 
